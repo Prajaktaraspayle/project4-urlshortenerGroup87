e8bbe184c726908f383c83209f656f59a93bd32e
Prajakta Prashant Raspayle <praspayle@gmail.com>
2022-05-20 01:49:10 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const urlController= require("../controllers/urlcontroller")
 
 
-router.post("/url/shorten",urlController.shortUrl )
-router.get("/:urlCode",urlController.getShortUrl)
+router.post("/url/shorten",urlController.shortenUrl )
+router.get("/:urlCode",urlController.redirectUrl)
 
 module.exports = router;
